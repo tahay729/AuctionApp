6d48f1d405208f71345f01e702e50048c8ef887c
Yousuft414 <yousuft@sheridancollege.ca>
2024-10-05 15:47:05 -0400

    Add project files.

---
+++ AuctionApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using AuctionApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Listing> Listings { get; set; }
+        public DbSet<Bid> Bids { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
